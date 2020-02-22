ddc433244e25cb4f14c4c42edacba46bfca2a215
shreeyanshbafna <56462471+shreeyanshbafna@users.noreply.github.com>
2020-02-22 08:33:47 +0000

    Update a.py
    Updated python file

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
 import time
 print("hello world i am back")
-time.sleep('3')
+time.sleep(3)
 print("hello google")
 for x in range(10):
   print("Hello coder")
-  time.sleep("2")
+  time.sleep(2)
   
